b8aa6b1c323d2fe93ee3d741eacbd9a205166e10
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-16 22:26:44 -0300

    Refactor Footer component and update layout

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full py-4 bg-white flex flex-col items-center">
+    <div className="w-full py-4 bg-white flex flex-col items-center mt-4">
       <h4 className="text-2xl">www.sitename.com</h4>
       <p className="text-base">Version</p>
     </div>
