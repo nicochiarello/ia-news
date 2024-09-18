476ef1f9a0e948efcc0a758cb93e6c30d520bc3d
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-18 16:22:44 -0300

    Refactor news retrieval and database connection in [category] page component

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,22 +1,56 @@
 import CategoryList from "../components/category-list/CategoryList";
 import FeaturedNews from "./components/featured-news/FeaturedNews";
+import mongoose from "mongoose";
+import { News } from "../api/news/create/route";
+import { categories } from "@/utils/categories";
+import { normalizeString } from "@/utils/normalizeString";
+
+// Conexión a MongoDB
+const connectDB = async () => {
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("Conectado a MongoDB");
+    } catch (error) {
+      console.error("Error conectando a MongoDB:", error);
+    }
+  }
+};
+
+// Función para manejar la solicitud GET
+export async function getNews(category: string) {
+  // Conectar a la base de datos
+  await connectDB();
+
+  try {
+    // Buscar todas las noticias que coincidan con la categoría
+    const foundCategory = categories.find(
+      (cat) => normalizeString(cat.name) === category
+    );
+
+    if (!foundCategory) {
+      throw new Error("Categoría no encontrada");
+    }
+
+    const newsByCategory = await News.find({
+      category: foundCategory.name,
+    }).sort({
+      createdAt: -1,
+    });
+
+    if (newsByCategory.length === 0) {
+      throw new Error("No se encontraron noticias para esta categoría");
+    }
+
+    // Devolver las noticias encontradas
+    return newsByCategory;
+  } catch (error) {
+    throw new Error("Error al buscar noticias");
+  }
+}
 
 // once a day
 export const revalidate = 86400;
-
-export const getNews = async (category: string) => {
-  const response = await fetch(
-    `${process.env.DB_URI}/api/news/category/${category}/get`,
-    {
-      next: {
-        revalidate: 0,
-      },
-    }
-  );
-  const data = await response.json();
-
-  return data.news;
-};
 
 export default async function Home({
   params,
