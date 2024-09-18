8636e68a20f0f58137e10851d6705a602f10ee38
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-18 16:22:47 -0300

    Refactor database connection and news retrieval in [name] page component

--- app/posts/[name]/page.tsx
+++ app/posts/[name]/page.tsx
@@ -1,24 +1,54 @@
 import Image from "next/image";
 import RelatedNewsCard from "./components/RelatedNewsCard";
+import mongoose from "mongoose";
+import { News } from "@/app/api/news/create/route";
 
-export async function generateStaticParams() {
-  const data: { news: Record<string, string>[] } = await fetch(
-    `${process.env.DB_URI}/api/news/get`
-  ).then((res) => res.json());
+// Conexión a MongoDB
+const connectDB = async () => {
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("Conectado a MongoDB");
+    } catch (error) {
+      console.error("Error conectando a MongoDB:", error);
+    }
+  }
+};
 
-  return data.news.map((post) => ({
-    id: post._id,
-  }));
+export async function getPost(id: string) {
+  // Conectar a la base de datos
+  await connectDB();
+
+  try {
+    // Buscar la noticia por el slug
+    const decodedId = decodeURIComponent(id);
+    const news = await News.findOne({ slug: decodedId });
+
+    if (!news) {
+      throw new Error(`Noticia ${id} no encontrada`);
+    }
+
+    // Buscar las 5 últimas noticias (ordenadas por fecha de creación)
+    const latestNews = await News.find({})
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .select("title slug imageUrl createdAt");
+
+    // Devolver la noticia encontrada, noticias relacionadas y las últimas 10 noticias
+    return {
+      news: news,
+      latestNews,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function Page({ params }: { params: { name: string } }) {
-  const response = await fetch(
-    `${process.env.DB_URI}/api/news/${params.name}/info`
-  ).then((res) => res.json());
+  const response = await getPost(params.name);
 
-  const post = response.data.news;
-  const latestPosts = response.data.latest;
-
+  const post = response?.news;
+  const latestPosts = response?.latestNews || [];
   return (
     <div className="flex w-full flex-col md:flex-row md:flex-1 gap-12 lg:gap-[6rem] my-8">
       <div className="flex-1 flex flex-col gap-6">
