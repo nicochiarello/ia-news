8404909a4af5d7c1f8fb80b41d6d5daa954de607
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-18 13:33:12 -0300

    Refactor Navbar component to include home link and also was added next top loader

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
     <div className={`flex py-4 justify-center items-center bg-white`}>
-      <div className="flex flex-col items-center">
+      <Link href={"/"} className="flex flex-col items-center">
         <h1 className="text-5xl font-thin">
           <span className="text-primary font-semibold">IA</span> news
         </h1>
         <h1 className="text-2xl">AI-generated news</h1>
-      </div>
+      </Link>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import { Alumni_Sans } from "next/font/google";
 import Footer from "./components/footer/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const AlumniSans = Alumni_Sans({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body
         className={`flex flex-col min-h-screen h-fit w-screen overflow-x-hidden antialiased bg-gray-200 text-black ${AlumniSans.className}`}
       >
+        <NextTopLoader color="#8D3AE2" />
         <Navbar />
         <div className="flex flex-1 min-h-full w-full max-w-[1050px] mx-auto px-2">{children}</div>
         <Footer />
