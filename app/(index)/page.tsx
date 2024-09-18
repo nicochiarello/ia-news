762befb10800403dafbebf439edd6b00f9044371
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2024-09-18 16:22:41 -0300

    Refactor news retrieval and database connection in Home page component

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,16 +1,31 @@
 import CategoryList from "../components/category-list/CategoryList";
 import FeaturedNews from "./components/featured-news/FeaturedNews";
 import LatestNews from "./components/latest-news/LatestNews";
+import mongoose from "mongoose";
+import { News } from "../api/news/create/route";
 
-export const getNews = async () => {
-  const response = await fetch(`${process.env.DB_URI}/api/news/get`, {
-    next: {
-      revalidate: 0,
-    },
-  });
-  const data = await response.json();
+// Conexión a MongoDB
+const connectDB = async () => {
+  if (mongoose.connection.readyState === 0) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("Conectado a MongoDB");
+    } catch (error) {
+      console.error("Error conectando a MongoDB:", error);
+    }
+  }
+};
 
-  return data.news;
+export const getNews = async (): Promise<Record<string, string>[]> => {
+  try {
+    await connectDB(); // Conectar a la base de datos
+
+    const news = await News.find({}); // Obtener todas las noticias
+
+    return news;
+  } catch {
+    throw new Error("Error al obtener las noticias");
+  }
 };
 
 // once a day
@@ -23,7 +38,7 @@ export default async function Home() {
     <div className="flex flex-1 flex-col">
       <CategoryList />
       <FeaturedNews featured={newsReponse.slice(0, 4)} />
-      <LatestNews news={newsReponse.slice(4,25)} />
+      <LatestNews news={newsReponse.slice(4, 25)} />
     </div>
   );
 }
